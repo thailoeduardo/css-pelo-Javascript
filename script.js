62fe499927e95fc134bc4c4037c6de0a2e7ef090
thailoeduardo <thailoeduardo@hotmail.com>
2016-12-10 21:36:47 -0200

    Page script

---
+++ script.js
@@ -0,0 +1,7 @@
+var linkCss = document.createElement('link');
+
+linkCss.setAttribute('rel', 'stylesheet');
+linkCss.setAttribute('type', 'text/css');
+linkCss.setAttribute('href', 'style.css');
+
+document.head.appendChild(linkCss);
